e8328745f475593f2c4b4b4fe0a223f12af0b610
abdallh224 <el7g00@gmail.com>
2022-12-22 00:17:50 +0200

    add images

--- src/tests/businessLogic/businessLogicSpec.ts
+++ src/tests/businessLogic/businessLogicSpec.ts
@@ -3,7 +3,7 @@ import imageResize from '../../businessLogic/imageResize'
 describe('Test image resize class', () => {
     //check if file exist return true
     it('check if file exist must return true', () => {
-        const IR = new imageResize(100, 100, 'abdo')
+        const IR = new imageResize(100, 100, 'tree')
         expect(IR.checkIfOutFileExist()).toBe(true)
     })
 })
