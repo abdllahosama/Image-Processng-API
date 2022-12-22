0f92d525d689ff73a7cb3cfbc1d917758535f3b7
‪abdallah Osama <51861082+abdllahosama@users.noreply.github.com>
2022-12-22 09:45:56 +0200

    add types

--- src/tests/businessLogic/businessLogicSpec.ts
+++ src/tests/businessLogic/businessLogicSpec.ts
@@ -1,8 +1,8 @@
 import imageResize from '../../businessLogic/imageResize'
 
-describe('Test image resize class', () => {
+describe('Test image resize class', (): void => {
     //check if file exist return true
-    it('check if file exist must return true', () => {
+    it('check if file exist must return true', (): Promise<void> => {
         const IR = new imageResize(100, 100, 'tree')
         expect(IR.checkIfOutFileExist()).toBe(true)
     })
