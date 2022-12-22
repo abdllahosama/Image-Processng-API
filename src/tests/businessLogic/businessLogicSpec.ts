92e9597c1aa14e00a2e1594b38f19b62cc17ffe4
‪abdallah Osama <51861082+abdllahosama@users.noreply.github.com>
2022-12-22 09:48:15 +0200

    Update businessLogicSpec.ts

--- src/tests/businessLogic/businessLogicSpec.ts
+++ src/tests/businessLogic/businessLogicSpec.ts
@@ -2,8 +2,11 @@ import imageResize from '../../businessLogic/imageResize'
 
 describe('Test image resize class', (): void => {
     //check if file exist return true
-    it('check if file exist must return true', (): Promise<void> => {
+    it('check if file exist must return true', async (): Promise<void> => {
+        //create image for test if not exist
         const IR = new imageResize(100, 100, 'tree')
+        await IR.resizeImage()
+
         expect(IR.checkIfOutFileExist()).toBe(true)
     })
 })
